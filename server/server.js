fec9243016c80f482540cdd2274b7e7e6244ec6b
mr-fahad-03 <mdfahadzulfiqar@gmail.com>
2025-07-14 03:21:28 -0700

    UPdated Server

--- server/server.js
+++ server/server.js
@@ -42,7 +42,7 @@ const app = express()
 // // CORS configuration
 app.use(cors({
    origin: 'https://graba2z-frontend.vercel.app',
-    origin: 'http://localhost:3000',
+   // origin: 'http://localhost:3000',
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin'],
   exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
