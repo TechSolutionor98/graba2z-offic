e54d957da5bc4d2759060735272024029e53055a
saleemmughal17 <saleem177mughal@gmai.com>
2025-07-16 18:21:12 +0500

    Server Fixed

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -95,7 +95,7 @@ const ContactUs = () => {
             <div>
               <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-lime-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                 disabled={loading}
               >
                 {loading ? "Submitting..." : "Send Request"}
