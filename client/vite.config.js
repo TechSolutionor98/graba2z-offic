83f0f2049866528a8cba8749e367b347cc245307
TechSolutionor98 <techsolutionor98@users.noreply.github.com>
2026-03-03 14:33:03 +0500

    Sitemap

--- client/vite.config.js
+++ client/vite.config.js
@@ -18,6 +18,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
+      '/sitemap.xml': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
